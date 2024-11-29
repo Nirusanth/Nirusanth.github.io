7eded69e14a71ed55cb9c59420b30fab5bf9671e
Nirusanth <nravishankar@torontomu.ca>
2024-11-29 02:33:36 -0500

    Update
    new

--- NiruMain.js
+++ NiruMain.js
@@ -10,9 +10,37 @@ function imageSizeUp() {
     }
 }
 
+//Contact Form
+document.getElementById("inquiryForm").addEventListener("submit", function (e) {
+    e.preventDefault();
 
+    const formInfo = {
+        Name: document.getElementById("Name").value,
+        phone: document.getElementById("phone").value,
+        email: document.getElementById("email").value,
+        inquiry: document.querySelector('input[name="inquiry"]:checked').value,
+        statement: document.getElementById("statement").value
+    };
 
+    localStorage.setItem("userFormData", JSON.stringify(formInfo));
+    document.getElementById("output").textContent = "Form submitted successfully!";
+});
 
+function formReset() {
+    // clear localStorage 
+    localStorage.removeItem("userFormData");
+    
+    document.getElementById("output").textContent = "Reset";
+}
  
+//Accessibility button
+function increaseSize() {
+    document.body.classList.toggle("access-txt");
+    if (body.classList.contains("access-txt")) {
+        body.classList.remove("access-txt"); 
+    } else {
+        body.classList.add("access-txt"); 
+    }
+}
 
 
